e6875cfbab30b01318c3d1dc3cc578c06c324594
Raymond McDonald <contact@wreckingballpublishing.com>
2024-02-05 17:19:18 -0600

    Complete warmup exercise 4 (but sometimes the same commands work right and sometimes they don't; why?)

--- warmup_exercises/exercise4.asm
+++ warmup_exercises/exercise4.asm
@@ -5,8 +5,13 @@
 Start:
     lda #100
 
+    clc ; Always clear the carry flag before calling adc
     adc #5
+
+    sec ; Always set the carry flag before calling sbc
     sbc #10
+
+    jmp Start ; Get in the habit of using an infinite loop to scrutinize instructions
 
     org $FFFC
     .word Start
