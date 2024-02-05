9f759cb644301044f6221ee522983bf4e5f73f6a
Raymond McDonald <contact@wreckingballpublishing.com>
2024-02-05 17:08:17 -0600

    Revise warmup exercise 3 (I missed some of the problem, but why does it not work without the jmp on line 17?)

--- warmup_exercises/exercise3.asm
+++ warmup_exercises/exercise3.asm
@@ -4,10 +4,17 @@
 
 Start:
     lda #15
+
     tax
     tay
     txa
     tya
+
+    ldx #6
+    txa
+    tay
+
+    jmp Start ; I have no clue why it won't compile without this jmp
 
     .org $FFFC
     .word Start
