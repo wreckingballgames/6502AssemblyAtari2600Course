57b78e2cacc838980ee1f6c4d61c35a342ac7939
Raymond McDonald <contact@wreckingballpublishing.com>
2024-02-05 17:34:55 -0600

    Complete warmup exercise 5 (note I only clear the carry flag before the adc in series, not every single adc)

---
+++ warmup_exercises/exercise5.asm
@@ -0,0 +1,24 @@
+    processor 6502
+    seg Code
+    org $F000
+
+Start:
+    lda #$A
+    ldx #%1010
+
+    sta $80
+    stx $81
+
+    lda #10
+
+    clc
+    adc $80
+    adc $81
+
+    sta $82
+
+    jmp Start
+
+    org $FFFC
+    .word Start
+    .word Start
